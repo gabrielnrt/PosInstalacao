11dda6b953bba10c394104624f5878200c03793d
gabrielnrt <gabrielpv98@gmail.com>
2021-07-28 11:49:02 -0300

    Arquivo pós instalação

---
+++ PosInstalacao.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+# Aplicativos pra instalar pela loja:
+
+# Spotify
+# Atom
+# WhatsApp
+
+#-----------------------------------------------------------------------------
+
+# PACOTES SÓ COM O APT
+
+sudo apt install python3-seaborn
+
+# O seaborn já instala o pandas
+# Caso a gente queira instalar esse pacote separadamente, é só fazer:
+
+#sudo apt install python3-pandas
+
+# O pandas por sua vez já instala o numpy, scipy e matplotlib.
+# Caso a gente queira instalar esses pacotes separadamente, é só fazer:
+
+#sudo apt install python3-matplotlib
+#sudo apt install python3-numpy
+#sudo apt install python3-scipy
+
+
+#-----------------------------------------------------------------------------
+
+# PACOTES COM O PIP
+
+sudo apt install python3-pip
+pip3 install -U scikit-learn
+pip3 install cvxpy
+
+sudo apt update
+sudo apt upgrade
